8bd2d2ede482111e1bdc0efabe4d9547cd93ea07
Kyrylo Bakumenko <44657125+Kyrylo-Bakumenko@users.noreply.github.com>
2024-04-09 21:14:52 -0400

    favicon test

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 import autoprefixer from 'autoprefixer';
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
+// const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 export default defineConfig({
   plugins: [
     eslint(),
+    // new FaviconsWebpackPlugin('src/imgs/bird-favicon.png'),
+    vitePluginFaviconsInject('src/imgs/bird-favicon.png'),
   ],
   css: {
     postcss: {
